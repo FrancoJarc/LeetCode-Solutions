2ddc28bdc190fc80df8d62db517bb7935e63ba92
FrancoJarc <115575015+FrancoJarc@users.noreply.github.com>
2026-06-02 13:54:39 -0300

    Time: 1308 ms (62.30%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,25 +1,23 @@
-WITH highest_average AS(
-    SELECT mv.movie_id,
-           m.title,
-           avg(mv.rating) AS hg
-    FROM MovieRating mv
-    INNER JOIN Movies m
-        ON mv.movie_id = m.movie_id
-    WHERE created_at >= '2020-02-01' AND created_at < '2020-03-01'
-    GROUP BY mv.movie_id
-    ORDER BY hg desc, m.title asc
+(
+    SELECT u.name as results
+    FROM Users u
+    INNER JOIN MovieRating mv
+    ON u.user_id = mv.user_id
+    GROUP BY mv.user_id
+    ORDER BY count(*) desc, u.name ASC
+    LIMIT 1
 )
 
-(SELECT u.name as results
-FROM Users u
-INNER JOIN MovieRating mv
-ON u.user_id = mv.user_id
-GROUP BY mv.user_id
-ORDER BY count(*) desc, u.name ASC
-LIMIT 1)
 
 UNION ALL
 
-(SELECT title
-FROM highest_average
-LIMIT 1)
+(    
+    SELECT m.title
+    FROM MovieRating mv
+    INNER JOIN Movies m
+    ON mv.movie_id = m.movie_id
+    WHERE created_at >= '2020-02-01' AND created_at < '2020-03-01'
+    GROUP BY mv.movie_id
+    ORDER BY avg(mv.rating) desc, m.title asc
+    LIMIT 1
+)
